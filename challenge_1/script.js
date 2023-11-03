b085a924c68d9caf98845ac1c669589dfe7f7612
Dewald Breed <brddew8@protonmail.com>
2023-11-03 11:52:12 +0200

    Completed Challenge 1

--- challenge_1/script.js
+++ challenge_1/script.js
@@ -1,34 +1,34 @@
 // script.js
 
-const 1-root = document(order1),
-const 1-biscuits: document(biscuits),
-const 1-donuts: document(donuts),
-const 1-pancakes: document(pancakes),
-const 1-status: document(status)
+const root_1 = document.querySelector('[data-key="order1"]')
+const biscuits_1 = document.querySelector('[data-key="order1"] .biscuits .count')
+const donuts_1 = document.querySelector('[data-key="order1"] .donuts .count')
+const pancakes_1 = document.querySelector('[data-key="order1"] .pancakes .count')
+const status_1 = document.querySelector('[data-key="order1"] .status dd')
 
-const 2-root = document(order2),
-const 2-biscuits: document(biscuits),
-const 2-donuts: document(donuts),
-const 2-pancakes: document(pancakes),
-const 2-status: document(status)
+const root_2 = document.querySelector('[data-key="order2"]')
+const biscuits_2 = document.querySelector('[data-key="order2"] .biscuits .count')
+const donuts_2 = document.querySelector('[data-key="order2"] .donuts .count')
+const pancakes_2 = document.querySelector('[data-key="order2"] .pancakes .count')
+const status_2= document.querySelector('[data-key="order2"] .status dd')
 
-const 3-root = document(order3),
-const 3-biscuits: document(biscuits),
-const 3-donuts: document(donuts),
-const 3-pancakes: document(pancakes),
-const 3-status: document(status)
+const root_3 = document.querySelector('[data-key="order3"]')
+const biscuits_3 = document.querySelector('[data-key="order3"] .biscuits .count')
+const donuts_3 = document.querySelector('[data-key="order3"] .donuts .count')
+const pancakes_3 = document.querySelector('[data-key="order3"] .pancakes .count')
+const status_3 = document.querySelector('[data-key="order3"] .status dd')
 
-1-biscuits= 1-root.biscuits,
-1-donuts = 1-root.donuts,
-1-pancakes = 1-root.pancakes,
-1-status = 1-root.status ? Delivered : Pending
+biscuits_1.innerText = root_1.dataset.biscuits
+donuts_1.innerText = root_1.dataset.donuts
+pancakes_1.innerText = root_1.dataset.pancakes
+status_1.innerText = root_1.dataset.delivered === "true" ? "Delivered" : "Pending"
 
-2-biscuits= 2-root.biscuits,
-2-donuts = 2-root.donuts,
-2-pancakes = 2-root.pancakes,
-2-status = 2-root.status ? Delivered : Pending
+biscuits_2.innerText = root_2.dataset.biscuits,
+donuts_2.innerText = root_2.dataset.donuts,
+pancakes_2.innerText = root_2.dataset.pancakes,
+status_2.innerText = root_2.dataset.delivered === "true" ? "Delivered" : "Pending"
 
-3-biscuits= 3-root.biscuits,
-3-donuts = 3-root.donuts,
-3-pancakes = 3-root.pancakes,
-3-status = 3-root.status ? Delivered : Pending
+biscuits_3.innerText = root_3.dataset.biscuits,
+donuts_3.innerText = root_3.dataset.donuts,
+pancakes_3.innerText = root_3.dataset.pancakes,
+status_3.innerText = root_3.dataset.delivered === "true" ? "Delivered" : "Pending"
